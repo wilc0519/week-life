4a52d8e43c263da1d009648f0e78ecf6760a7ee5
wilc0519 <wilc0519@gmail.com>
2021-09-01 16:11:41 -0500

    removing unnecesary files

---
+++ routes/userRoute.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../database/models/user')
+
+router.post('/users', async (req, res) => {    
+    try {
+        const userEmail = await User.findOne({
+            where: {
+                email: req.body.email
+            }
+        });
+        
+        if (userEmail === null) {
+            const firstName = req.body.firstName;
+            const lastName = req.body.lastName;
+            const email = req.body.email
+            const user = await User.create({ firstName, lastName, email })
+            res.status(201).send(user);
+        } else {
+            res.status(400).send({error:'User already exists'})
+        }
+    } catch (e) {
+        res.status(500).send(e);
+    }
+})
+router.put('/users', async (req, res) => {
+    try {
+        const user = await User.findOne({where:{email:req.body.email}})
+        await user.update({ dateOfBirth: req.body.dateOfBirth }
+        )
+        res.status(200).send(user)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+router.get('/users?email=myemail', async(req, res)=>{
+    
+
+
+    try {
+        const user = await User.findOne({
+            where:{
+                email:
+            }
+        })
+        res.status(200).send(user)
+    } catch (error) {
+        res.status(500).send(e)
+    }
+})
+module.exports = router;
--- week-life-front-end/src/pages/NumberOfWeeks.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const NumberOfWeeks = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
