8923e814ebdecf22a0748cc705b5b7fd269ceb63
wilc0519 <wilc0519@gmail.com>
2021-09-03 13:19:18 -0500

    modidy field user by email 3

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -28,7 +28,7 @@ router.put('/users/:user_id',async(req,res)=>{
     const isValidOperation = updates.every((update)=>allowedUpdates.includes(update))
 
     if(!isValidOperation){
-        return res.status(400).send({error:'Invalid update'}); 
+        res.status(400).send({error:'Invalid update'}); 
     }
     
     try{
@@ -52,7 +52,7 @@ router.get('/users', async (req, res)=>{
         if(user){
             res.status(200).send(user);
         }
-        return res.status(404).send({error:'User not found'});
+        res.status(404).send({error:'User not found'});
                 
     } catch (e) {
         res.status(500).send(e);
