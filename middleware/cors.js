df8a13695279ed9a1fc33850e3b5b7007d6bd82e
wilc0519 <wilc0519@gmail.com>
2021-09-07 22:24:43 -0500

    task to run eslint

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,16 +1,16 @@
-const cors = ()=> {
-    return function (req, res, next) {
-        res.header('Access-Control-Allow-Origin', '*')
-        res.header('Access-Control-Max-Age', '1000')
-        res.header(
-            'Access-Control-Allow-Methods',
-            'POST, GET, DELETE, PUT'
-        )
-        res.header(
-            'Access-Control-Allow-Headers',
-            'X-Requested-With, Content-Type, Origin, Authorization, Accept, Accept-Encoding'
-        )
-        next()
-    }
+const cors = () => {
+  return function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Max-Age', '1000')
+    res.header(
+      'Access-Control-Allow-Methods',
+      'POST, GET, DELETE, PUT'
+    )
+    res.header(
+      'Access-Control-Allow-Headers',
+      'X-Requested-With, Content-Type, Origin, Authorization, Accept, Accept-Encoding'
+    )
+    next()
+  }
 }
-module.exports=cors;
+module.exports = cors
