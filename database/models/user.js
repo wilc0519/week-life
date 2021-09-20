3b3ef7f635624391801371c618eee4617eaa27fc
wilc0519 <wilc0519@gmail.com>
2021-09-20 16:33:20 -0500

    environment

--- database/models/user.js
+++ database/models/user.js
@@ -2,17 +2,16 @@ const { Model, DataTypes } = require('sequelize')
 const { sequelize } = require('../../models/index')
 
 class User extends Model {
-  static associate (models) {
-    User.hasMany(models.Note, {
-      foreignKey: 'userId',
-      as: 'Notes'
-    })
-  }
 };
 User.init({
   firstName: DataTypes.STRING,
   lastName: DataTypes.STRING,
-  email: DataTypes.STRING,
+  email: {
+    type: DataTypes.STRING,
+    validate: {
+      isEmail: true
+    }
+  },
   dateOfBirth: DataTypes.DATEONLY
 }, {
   sequelize,
